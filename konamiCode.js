374d417aaa0118245a9d2d13c3da7f6090ff1649
Watermelon876 <yash.farooqui@gmail.com>
2015-10-15 22:12:35 +0000

    added default code options to konamiCode.js to simplify use

--- konamiCode.js
+++ konamiCode.js
@@ -47,3 +47,5 @@ function codeImagePair(combination, imageSrc) {
     document.addEventListener("keydown", eventListener, false);
 }
 
+var konami = [38,38,40,40,37,39,37,39,66,65];
+var vvvvvv = [86,86,86,86,86,86];
